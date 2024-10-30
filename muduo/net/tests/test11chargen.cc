8dbe746e742ea33d064266424bb6b986650a3348
Zhang1933 <193319076@qq.com>
2024-10-30 16:08:24 +0800

    s09

---
+++ muduo/net/tests/test11chargen.cc
@@ -0,0 +1,72 @@
+#include "muduo/base/Logging.h"
+#include "muduo/net/TcpServer.h"
+#include "muduo/net/EventLoop.h"
+#include "muduo/net/InetAddress.h"
+#include <cstddef>
+#include <stdio.h>
+
+std::string message;
+
+void onConnection(const muduo::TcpConnectionPtr& conn)
+{
+  if (conn->connected())
+  {
+    printf("onConnection(): new connection [%s] from %s\n",
+           conn->name().c_str(),
+           conn->peerAddress().toHostPort().c_str());
+    conn->send(message);
+  }
+  else
+  {
+    printf("onConnection(): connection [%s] is down\n",
+           conn->name().c_str());
+  }
+}
+
+ssize_t len=0;
+void onWriteComplete(const muduo::TcpConnectionPtr& conn)
+{
+  LOG_INFO<<"msg sended "<<len;
+  conn->send(message);
+  len+=message.size();
+}
+
+void onMessage(const muduo::TcpConnectionPtr& conn,
+               muduo::Buffer* buf,
+               muduo::Timestamp receiveTime)
+{
+  printf("onMessage(): received %zd bytes from connection [%s] at %s\n",
+         buf->readableBytes(),
+         conn->name().c_str(),
+         receiveTime.toFormattedString().c_str());
+
+  buf->retrieveAll();
+}
+
+int main()
+{
+  printf("main(): pid = %d\n", getpid());
+
+  std::string line;
+  for (int i = 33; i < 127; ++i)
+  {
+    line.push_back(char(i));
+  }
+  line += line;
+
+  for (size_t i = 0; i < 127-33; ++i)
+  {
+    message += line.substr(i, 72) + '\n';
+  }
+
+  muduo::InetAddress listenAddr(9981);
+  muduo::EventLoop loop;
+
+  muduo::TcpServer server(&loop, listenAddr);
+  server.setConnectionCallback(onConnection);
+  server.setMessageCallback(onMessage);
+  server.setWriteCompleteCallback(onWriteComplete);
+  server.start();
+
+  loop.loop();
+}
